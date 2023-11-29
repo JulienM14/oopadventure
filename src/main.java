f8aa493f6e042cec03dc30bad093186e59f40c9f
JulienM14 <jumedina14@outlook.com>
2023-11-29 09:12:28 -0500

    latest

--- src/main.java
+++ src/main.java
@@ -3,11 +3,15 @@ package src;
 import src.constants.WindowConstants;
 import javax.swing.*;
 
+import java.awt.event.*;
+
 class Main {
     public static JFrame window = new JFrame("Game");
+    
 
     public static void main(String[] args) {
         windowCreator();
+        
     }
 
     public static void windowCreator() {
@@ -20,6 +24,12 @@ class Main {
         }
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.setSize(WindowConstants.windowWidth, WindowConstants.windowHeight);
+        window.addKeyListener(new KeyAdapter() {
+            public void keyPressed(KeyEvent e) {
+
+            }
+        });
         window.setVisible(true);
     }
 }
+
