cc0dfbfa35ff91a6951031cd2e7043eeb2db9658
JulienM14 <jumedina14@outlook.com>
2023-11-30 09:31:09 -0500

    latest things

--- src/main.java
+++ src/main.java
@@ -18,6 +18,7 @@ import java.io.*;
 import javax.sound.sampled.*;
 @SuppressWarnings("unused")
 class Main {
+    public static final boolean printKeyPresses = true;
     public static JFrame window = new JFrame("Game");
     public static JLabel textOut = new JLabel("Null");
     public Player player = new Player("Testing");
@@ -46,22 +47,21 @@ class Main {
         }
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.setSize(WindowConstants.windowWidth, WindowConstants.windowHeight);
-        window.add(textOut);
         window.addKeyListener(new KeyAdapter() {
             public void keyPressed(KeyEvent e) {
                 int keyCode = e.getKeyCode();
                 switch (keyCode) {
                     case 87:
-                        p.setLocation(p.getX(), (p.getY() - 10));
+                        keyPress(1);
                         break;
                     case 65:
-                        p.setLocation(p.getX() - 10, p.getY());
+                        keyPress(2);
                         break;
                     case 83:
-                        p.setLocation(p.getX(), (p.getY() + 10));
+                        keyPress(3);
                         break;
                     case 68:
-                        p.setLocation(p.getX() + 10, (p.getY()));
+                        keyPress(4);
                         break;
                     default:
                         break;
@@ -89,6 +89,41 @@ class Main {
         g.drawImage(image, 100, 100, p);
     }
 
+    /**
+     * Method used for keypresses in switchcase
+     * @param key Key pressed, W=1, A=2, S=3, D=4
+     */
+    public static void keyPress(int key) {
+        if (key == 1) {
+            p.setLocation(p.getX(), (p.getY() - 10));
+            if (printKeyPresses) {
+                System.out.println("W Pressed");
+            }
+        }
+        else if (key == 2) {
+            p.setLocation(p.getX() - 10, p.getY());
+            if (printKeyPresses) {
+                System.out.println("A Pressed");
+            }
+        }
+        else if (key == 3) {
+            p.setLocation(p.getX(), (p.getY() + 10));
+            if (printKeyPresses) {
+                System.out.println("S Pressed");
+            }
+        }
+        else if (key == 4) {
+            p.setLocation(p.getX() + 10, (p.getY()));
+            if (printKeyPresses) {
+                System.out.println("D Pressed");
+            }
+        }
+        else {
+            System.out.println("Something broke with keypresses!");
+            System.exit(0);
+        }
+
+    }
 }
 
 class Player {
