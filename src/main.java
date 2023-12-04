4d60dd86acb4e63ba05379f2bfb6e7bd572c6f6c
JulienM14 <jumedina14@outlook.com>
2023-12-04 09:10:15 -0500

    screaming

--- src/main.java
+++ src/main.java
@@ -1,8 +1,15 @@
 package src;
 
+import java.util.*;
+
 class main {
-    String[] wordBank = {"Apple", "Banana", "Peach", "Grape", "Highway", "Computer","Seesaw","Piano","Robot","Czecholslovakia","Carrot","School","Door","Doctor","Teacher","Coding","DrPepper","Wales", "High School", "School"};
+    String[] wordBankEasy = {"Apple", "Banana", "Peach", "Grape", "Highway", "Computer","Seesaw","Piano","Robot","Czecholslovakia","Carrot","School","Door","Doctor","Teacher","Coding","DrPepper","Wales", "High School", "School"};
+    String[] wordBankMedium
+    String[] wordBankHard 
     public static void main(String[] args) {
         
+    }
+    public String wordSelector() {
+        return wordBank[]
     }
 }
