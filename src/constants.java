592994eb5e489a20dfd5a2c76f260be448b21c3f
JulienM14 <jumedina14@outlook.com>
2023-12-04 08:55:46 -0500

    giving up

--- src/constants.java
+++
@@ -1,8 +0,0 @@
-package src;
-public class constants {
-    public class WindowConstants {
-        public final static int buttonSize = 600;
-        public final static int windowWidth = 1280;
-        public final static int windowHeight = 720; 
-    }
-}
