70fd569e4b71f51af2b9baa71df816464af835eb
JulienM14 <jumedina14@outlook.com>
2023-12-04 09:07:33 -0500

    changes...

--- src/main.java
+++ src/main.java
@@ -1,7 +1,7 @@
 package src;
 
 class main {
-    String[] wordBank = {"Apple", "Banana", "Peach", "Grape", "Highway", "Computer"};
+    String[] wordBank = {"Apple", "Banana", "Peach", "Grape", "Highway", "Computer","Seesaw","Piano","Robot","Czecholslovakia","Carrot","School","Door","Doctor","Teacher","Coding","DrPepper","Wales", "High School", "School"};
     public static void main(String[] args) {
         
     }
