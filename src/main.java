478e0bbb33b15164da50c1affc6b87f33b7cb089
JulienM14 <jumedina14@outlook.com>
2023-12-04 09:24:07 -0500

    yelling and screaming

--- src/main.java
+++ src/main.java
@@ -3,13 +3,39 @@ package src;
 import java.util.*;
 
 class main {
-    String[] wordBankEasy = {"Apple", "Banana", "Peach", "Grape", "Highway", "Computer","Seesaw","Piano","Robot","Czecholslovakia","Carrot","School","Door","Doctor","Teacher","Coding","DrPepper","Wales", "High School", "School"};
-    String[] wordBankMedium
-    String[] wordBankHard 
+    public final static boolean debugMode = true;
+    public static final String[] wordBankEasy = {"Apple", "Banana", "Peach", "Grape", "Highway", "Computer","Seesaw","Piano","Robot","Czecholslovakia","Carrot","School","Door","Doctor","Teacher","Coding","DrPepper","Wales", "High School", "School"};
+    // public static final String[] wordBankMedium
+    // public static final String[] wordBankHard 
+    Random rand = new Random();
     public static void main(String[] args) {
         
     }
-    public String wordSelector() {
-        return wordBank[]
+
+    public String wordSelector(int difficulty) {
+        switch (difficulty) {
+            case 1: return wordBankEasy[rand.nextInt(0,10)];
+            // case 2: return wordBankMedium[rand.nextInt(0,10)]; break;
+            // case 3: return wordBankHard[rand.nextInt(0,10)]; break;
+            default: return null;
+        }
     }
+
+    public static String[] wordBreaker(String word) {
+        String[] brokenWord = new String[word.length()];
+        for (int i = 0; i <= word.length() - 1; i++) {
+            brokenWord[i] = word.substring(i,i+1);
+        }
+        if (debugMode) {
+            for (int i = 0; i < brokenWord.length; i++) {
+                System.out.println(brokenWord[i]);
+            }
+        }
+        return brokenWord;
+    }
+
+    public void game(String word) {
+
+    }
+
 }
