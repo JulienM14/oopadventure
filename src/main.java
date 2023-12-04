3a297a40b4b5e96dbf27b692811ff51fee349949
Kered138 <97762631+Kered138@users.noreply.github.com>
2023-12-04 09:31:34 -0500

    _

--- src/main.java
+++ src/main.java
@@ -5,8 +5,8 @@ import java.util.*;
 class main {
     public final static boolean debugMode = true;
     public static final String[] wordBankEasy = {"Apple", "Banana", "Peach", "Grape", "Highway", "Computer","Seesaw","Piano","Robot","Carrot","School","Door","Doctor","Teacher","Coding","Wales", "School"};
-    // public static final String[] wordBankMediumn = {"Bookbag","Highschool","Textbook","Certificate","Transparency","Information","Technology","Graduate","Periodic","Iterate","Feature","Component","Quantum"};
-    // public static final String[] wordBankHard = {"Czechoslovakia","HypertextTransferProtocol","Antiderivative","Kazakisthan","Familiarize","Flibbertigibbet","Unbiseptium","Rutherfordium"};
+    public static final String[] wordBankMedium = {"Bookbag","Highschool","Textbook","Certificate","Transparency","Information","Technology","Graduate","Periodic","Iterate","Feature","Component","Quantum"};
+    public static final String[] wordBankHard = {"Czechoslovakia","HypertextTransferProtocol","Antiderivative","Kazakisthan","Familiarize","Flibbertigibbet","Unbiseptium","Rutherfordium"};
     Random rand = new Random();
     public static void main(String[] args) {
         
@@ -15,8 +15,8 @@ class main {
     public String wordSelector(int difficulty) {
         switch (difficulty) {
             case 1: return wordBankEasy[rand.nextInt(0,10)];
-            // case 2: return wordBankMedium[rand.nextInt(0,10)]; break;
-            // case 3: return wordBankHard[rand.nextInt(0,10)]; break;
+            case 2: return wordBankMedium[rand.nextInt(0,10)];
+            case 3: return wordBankHard[rand.nextInt(0,10)]; 
             default: return null;
         }
     }
@@ -34,8 +34,11 @@ class main {
         return brokenWord;
     }
 
-    public void game(String word) {
-
+    public void game(String[] word) {
+        for(int i = 0; i<= word.length; i++){
+            System.out.println("_ ");
+        }
+        
     }
 
 }
